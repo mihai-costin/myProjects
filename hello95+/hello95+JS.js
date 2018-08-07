78d0fe8833189bca634ad2de7fb7355d623ddff3
Gafencu Mihai-Costin <gafencumihai@yahoo.com>
2018-08-07 17:42:36 +0300

    Add files via upload

---
+++ hello95+/hello95+JS.js
@@ -0,0 +1,438 @@
+var obj = [{
+    lang: "Afrikaans",
+    hello: "Hello Wêreld!",
+    value: 0
+}, { 
+    lang: "Albanian",
+    hello: "Përshendetje Botë!",
+    value: 0
+}, {
+    lang: "Amharic",
+    hello: "ሰላም ልዑል!",
+    value: 0
+}, {
+    lang: "Armenian",
+    hello: "Բարեւ աշխարհ!",
+    value: 0
+},{
+    lang: "Azerbajani",
+    hello: "Salam dünya!",
+    value: 0
+}, {
+    lang: "Basque",
+    hello: "Kaixo Mundua!",
+    value: 0
+}, {
+    lang: "Belarusian",
+    hello: "Прывітанне Сусвет!",
+    value: 0
+}, {
+    lang: "Bengali",
+    hello: "ওহে বিশ্ব,!",
+    value: 0
+}, {
+    lang: "Bosnian",
+    hello: "Zdravo svijet!",
+    value: 0
+}, {
+    lang: "Bulgarian",
+    hello: "Здравей свят!",
+    value: 0
+}, {
+    lang: "Catalan",
+    hello: "Hola món!",
+    value: 0
+}, {
+    lang: "Cebuano",
+    hello: "Hello World!",
+    value: 0
+}, {
+    lang: "Chichewa",
+    hello: "Moni Dziko Lapansi!",
+    value: 0
+}, {
+    lang: "Chinese",
+    hello: "你好，世界!",
+    value: 0
+}, {
+    lang: "Corsican",
+    hello: "Hello World!",
+    value: 0
+}, {
+    lang: "Croatian",
+    hello: "Pozdrav svijete!",
+    value: 0
+}, {
+    lang: "Czech",
+    hello: "Ahoj světe!",
+    value: 0
+}, {
+    lang: "Danish",
+    hello: "Hej Verden!",
+    value: 0
+}, {
+    lang: "Dutch",
+    hello: "Hallo Wereld!",
+    value: 0
+}, {
+    lang: "English",
+    hello: "Hello World!",
+    value: 0
+}, {
+    lang: "Esperanto",
+    hello: "Saluton Mondo!",
+    value: 0
+}, {
+    lang: "Estonian",
+    hello: "Tere maailm!",
+    value: 0
+}, {
+    lang: "Filipino",
+    hello: "Hello World!",
+    value: 0
+}, {
+    lang: "Finnish",
+    hello: "Hei maailma!",
+    value: 0
+}, {
+    lang: "French",
+    hello: "Bonjour le monde!",
+    value: 0
+}, {
+    lang: "Frisian",
+    hello: "Hallo wrâld!",
+    value: 0
+}, {
+    lang: "Gallician",
+    hello: "¡Hola Mundo!",
+    value: 0
+}, {
+    lang: "Georgian",
+    hello: "გამარჯობა მსოფლიო!",
+    value: 0
+}, {
+    lang: "German",
+    hello: "Hallo Welt!",
+    value: 0
+}, {
+    lang: "Greek",
+    hello: "Γειά σου Κόσμε!",
+    value: 0
+}, {
+    lang: "Gujarati",
+    hello: "હેલો વર્લ્ડ!",
+    value: 0
+}, {
+    lang: "Haitian",    
+    hello: "Alo Mondyal!",
+    value: 0
+}, {
+    lang: "Hausa",
+    hello: "Sannu Duniya!",
+    value: 0
+}, {
+    lang: "Hawaiian",
+    hello: "Aloha World!",
+    value: 0
+}, {
+    lang: "Hindi",
+    hello: "नमस्ते दुनिया!",
+    value: 0
+}, {
+    lang: "Hmong",
+    hello: "Nyob zoo ntiaj teb!",
+    value: 0
+}, {
+    lang: "Hungarian",
+    hello: "Helló Világ!",
+    value: 0
+}, {
+    lang: "Icelandic",
+    hello: "Halló heimur!"
+}, {
+    lang: "Igbo",
+    hello: "Ndewo Ụwa!",
+    value: 0
+}, {
+    lang: "Indonesian",
+    hello: "Halo Dunia!",
+    value: 0
+}, {
+    lang: "Irish",
+    hello: "Dia duit Domhanda!",
+    value: 0
+}, {
+    lang: "Italian",
+    hello: "Ciao mondo!",
+    value: 0
+}, {
+    lang: "Japanese",
+    hello: "こんにちは世界!",
+    value: 0
+}, {
+    lang: "Javanese",
+    hello: "Hello World!",
+    value: 0
+}, {
+    lang: "Kannada",
+    hello: "ಹಲೋ ವರ್ಲ್ಡ್!",
+    value: 0
+}, {
+    lang: "Kazakh",
+    hello: "Сәлем Әлем!" ,
+    value: 0
+}, {
+    lang: "Khmer",    
+    hello: "សួស្តី​ពិភពលោក!",
+    value: 0
+}, {
+    lang: "Korean",
+    hello: "안녕하세요!",
+    value: 0
+}, {
+    lang: "Kurdish",
+    hello: "Hello World!",
+    value: 0
+}, {
+    lang: "Kyrgyz",
+    hello: "Салам дүйнө!",
+    value: 0
+}, {
+    lang: "Lao",
+    hello: "ສະ​ບາຍ​ດີ​ຊາວ​ໂລກ!",
+    value: 0
+}, {
+    lang: "Latin",
+    hello: "Orbis Terrarum salve!",
+    value: 0
+}, { 
+    lang: "Latvian",
+    hello: "Sveika pasaule!",
+    value: 0
+}, {
+    lang: "Lithuanian",
+    hello: "Labas pasauli!",
+    value: 0
+}, {
+    lang: "Luxembourgish",
+    hello: "Moien Welt!",
+    value: 0
+}, {
+    lang: "Macedonian",
+    hello: "Здраво свету!",
+    value: 0
+}, {
+    lang: "Malagasy",
+    hello: "Hello World!",
+    value: 0
+}, {
+    lang: "Malay",
+    hello: "Hai dunia!",
+    value: 0
+}, {
+    lang: "Malayalam",
+    hello: "ഹലോ വേൾഡ്!",
+    value: 0
+}, {
+    lang: "Maltese",
+    hello: "Hello dinja!",
+    value: 0
+}, {
+    lang: "Maori",
+    hello: "Hello World!",
+    value: 0
+}, {
+    lang: "Marathi",
+    hello: "हॅलो वर्ल्ड!",
+    value: 0
+}, {
+    lang: "Mongolian",
+    hello: "Сайн уу!",
+    value: 0
+}, {
+    lang: "Myanmar",
+    hello: "မင်္ဂလာပါကမ္ဘာလောက!",
+    value: 0
+}, {
+    lang: "Nepali",
+    hello: "नमस्कार संसार!",
+    value: 0
+}, {
+    lang: "Norwegian",
+    hello: "Hei Verden!",
+    value: 0
+}, {
+    lang: "Polish",
+    hello: "Witaj swiecie!",
+    value: 0
+}, {
+    lang: "Portugese",
+    hello: "Olá Mundo!",
+    value: 0
+}, {
+    lang: "Punjabi",
+    hello: "ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ ਦੁਨਿਆ!",
+    value: 0
+}, {
+    lang: "Romanian",
+    hello: "Salut Lume!",
+    value: 0
+}, {
+    lang: "Russian",
+    hello: "Привет мир!",
+    value: 0
+}, {
+    lang: "Samoan",
+    hello: "Talofa le Lalolagi!",
+    value: 0
+}, {
+    lang: "Scots Gaelic",
+    hello: "Hàlo a Shaoghail!",
+    value: 0
+}, {
+    lang: "Serbian",
+    hello: "Здраво Свете!",
+    value: 0
+}, {
+    lang: "Sesotho",
+    hello: "Lefatše Lumela!",
+    value: 0
+}, {
+    lang: "Shona",
+    hello: "Hello World!",
+    value: 0
+}, {
+    lang: "Sinhala",
+    hello: "හෙලෝ වර්ල්ඩ්!",
+    value: 0
+}, {
+    lang: "Slovak",
+    hello: "Hello World!",
+    value: 0
+}, {
+    lang: "Slovenian",
+    hello: "Pozdravljen, svet!",
+    value: 0
+}, {
+    lang: "Somali",
+    hello: "Hello World!",
+    value: 0
+}, {
+    lang: "Spanish",
+    hello: "Hola Mundo!",
+    value: 0
+}, {
+    lang: "Sundanese",
+    hello: "Halo Dunya!",
+    value: 0
+}, {
+    lang: "Swahili",
+    hello: "Salamu, Dunia!",
+    value: 0
+}, {
+    lang: "Swedish",
+    hello: "Hej världen!",
+    value: 0
+}, {
+    lang: "Tajik",
+    hello: "Салом Ҷаҳон!",
+    value: 0
+}, {
+    lang: "Tamil",
+    hello: "வணக்கம்!",
+    value: 0
+}, {
+    lang: "Telugu",
+    hello: "హలో వరల్డ్!",
+    value: 0
+}, {
+    lang: "Thai",
+    hello: "สวัสดีชาวโลก!",
+    value: 0
+}, {
+    lang: "Turksih",
+    hello: "Selam Dünya!",
+    value: 0
+}, {
+    lang: "Ukranian",
+    hello: "Привіт Світ!",
+    value: 0
+}, {
+    lang: "Uzbek",
+    hello: "Salom Dunyo!",
+    value: 0
+}, {
+    lang: "Vietnamese",
+    hello: "Chào thế giới!",
+    value: 0
+}, {
+    lang: "Welsh",
+    hello: "Helo Byd!",
+    value: 0
+}, {
+    lang: "Xhosa",
+    hello: "Molo Lizwe!",
+    value: 0
+}, {
+    lang: "Yoruba",
+    hello: "Mo ki O Ile Aiye!",
+    value: 0
+}, {
+    lang: "Zulu",
+    hello: "Sawubona Mhlaba!",
+    value: 0
+}]; // object for the hello in every language
+
+$(document).ready(function(){
+    for(var i = 0;i<obj.length;i++)
+         obj[i].value = i;
+    
+//randomize language
+    $("#btn1").click(function(){
+    var rdn = Math.floor(Math.random()*obj.length);
+    $("#hi").html(obj[rdn].hello);
+    $("#Lang").html(obj[rdn].lang);
+});
+    
+// search a language by typing
+    $("#btn").click(function(){
+    var ser = $("#serc"); // search field -- id for text input
+    var result = binarySearch(ser.value);
+    
+    if(result > -1){
+        $("#hi").html(obj[result].hello);
+        $("#lan").html(obj[result].lang);
+    }
+    else alert("The language wasn't found! Try try again!");
+});
+
+// for search field -- trigger the search when Enter key is pressed
+    $("serc").keyup(function(event){
+        event.preventDefault();
+        if(event.keyCode === 13)
+            $("#btn").click();
+    });
+});
+
+// binarySearch to search for a language when the user wants to search by typing
+function binarySearch(value){
+    value = value.toLowerCase();
+   
+    var first = 0, last = obj.length-1;
+    var mid = Math.floor((first+last)/2);
+    var check = false;
+    
+    do{
+        if(value === obj[mid].lang.toLowerCase()) check = true;
+        
+        else {    
+         if(value.localeCompare(obj[mid].lang.toLowerCase()) === 1)first = mid + 1;
+         else last = mid - 1;
+         
+         mid = Math.floor((first+last)/2);
+        }
+    }while(!check && first<=last);
+    
+    if(check) return mid;
+    return -1;
+}
