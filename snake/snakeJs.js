eca7ce360f2b94d616fa3bdb9efa61de31dcc62e
gafencumihai <gafencumihai@yahoo.com>
2018-08-23 14:29:58 -0400

    code snake!

--- snake/snakeJs.js
+++ snake/snakeJs.js
@@ -1,7 +1,8 @@
 var play = false;
 var dx = 100, dy = 100;
 var mx = 4, my = 0;
-var myvar;
+var myvar, rdnx, rdny;
+var score = 0, swidth = 50;
 
 function moveSnake(){
   var $myCanvas = $("#myCanvas");
@@ -10,14 +11,14 @@ function moveSnake(){
     fillStyle: "Black",
     x: dx, y: dy,
     fromCenter: false,
-    width: 50,
+    width: swidth,
     height: 10
   });
   $myCanvas.drawArc({
     fillStyle: "Grey",
     strokeStyle: "black",
     strokeWidth: 2,
-    x: 100, y: 50,
+    x: rdnx, y: rdny,
     radius: 5 
   });
   if(dx<0 || dx >400)
@@ -27,14 +28,16 @@ function moveSnake(){
   
   dx += mx;
   dy += my;
+  point();
 }
 
 function draw() {
 
 if(play === false) {
+  $(".scoreBox").show();
   play = true;
   $("body").on("keypress",function(event){
-   if(dx>0 && dx<600 && dy>0 && dy<600)
+   //if(dx>0 && dx<600 && dy>0 && dy<600)
     switch (event.keyCode) {
       case 56: {
         mx = 0;
@@ -58,11 +61,13 @@ if(play === false) {
       }      
     }
   });
+ randomize();
  myvar = setInterval(moveSnake,100);
  
 }
   else {
     play = false;
+    $(".scoreBox").hide();
     $("body").off("keypress");
     clearInterval(myvar);
   }
@@ -70,4 +75,18 @@ if(play === false) {
 
 function help() {
   $(".helpBox").toggle();
+}
+
+function randomize(){
+  rdnx = Math.floor(Math.random()*450);
+  rdny = Math.floor(Math.random()*450);
+}
+
+function point(){
+  if((dx<= rdnx+10 && dx>=rdnx-10) && (dy >= rdny-50 && dy<=rdny+50))
+    {
+      score++;
+      randomize();
+      $(".scoreBox").html("Score: " + score);
+    }
 }
